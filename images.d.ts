985aae2b0580b02bde118d70688f493fcacfe98f
phucvu1601 <phucvuhuu1601@gmail.com>
2026-08-25 19:07:51 +0700

    chore: fix deploy

---
+++ images.d.ts
@@ -0,0 +1,30 @@
+// Allow to import all file types supported by next/image
+declare module "*.svg" {
+  const content: import("next/image").StaticImageData
+  export default content
+}
+
+declare module "*.png" {
+  const content: import("next/image").StaticImageData
+  export default content
+}
+
+declare module "*.jpg" {
+  const content: import("next/image").StaticImageData
+  export default content
+}
+
+declare module "*.jpeg" {
+  const content: import("next/image").StaticImageData
+  export default content
+}
+
+declare module "*.webp" {
+  const content: import("next/image").StaticImageData
+  export default content
+}
+
+declare module "*.gif" {
+  const content: import("next/image").StaticImageData
+  export default content
+}
